188cf5563016c6a115c85e732473d942e896d95e
Minjoon Seo <seominjoon@gmail.com>
2016-02-13 17:40:07 -0800

    train test combined

--- main.py
+++ main.py
@@ -2,15 +2,14 @@ import json
 import os
 
 import tensorflow as tf
-import progressbar as pb
+from pprint import pprint
 
 from data import read_vqa
 from model import Model
 
 flags = tf.app.flags
 
-flags.DEFINE_integer("train_batch_size", 100, "Batch size during training [100]")
-flags.DEFINE_integer("val_batch_size", 100, "Batch size during validation [100]")
+# All the file directories... should get rid of these!
 flags.DEFINE_string("train_image_rep_h5", "train/image_rep.h5", "image_rep.h5 file path for training [train/image_rep.h5]")
 flags.DEFINE_string("train_image_idx", "train/image_idx.json", "image_idx.json file path for training [train/image_idx.json]")
 flags.DEFINE_string("train_sent_h5", "train/sent.h5", "sent.h5 file path for training [train/sent.h5]")
@@ -22,25 +21,34 @@ flags.DEFINE_string("val_sent_h5", "val/sent.h5", "sent.h5 file path for validat
 flags.DEFINE_string("val_label", "val/label.json", "label.json file path for validation [val/label.json]")
 flags.DEFINE_string("val_len", "val/len.json", "len.json file path for valing [val/len.json]")
 flags.DEFINE_string("vocab_dict", "val/vocab_dict.json", "vocab_dict.json file path [val/vocab_dict.json]")
+
+# training parameters
 flags.DEFINE_integer("num_epochs", 100, "Total number of epochs [100]")
+flags.DEFINE_integer("train_batch_size", 100, "Batch size during training [100]")
+flags.DEFINE_integer("val_batch_size", 100, "Batch size during validation [100]")
+flags.DEFINE_integer("rnn_num_layers", 3, "Number of RNN (LSTM) layers [3]")
+flags.DEFINE_integer("rnn_hidden_size", 512, "Hidden size of RNN (LSTM) [512]")
+flags.DEFINE_integer("common_size", 1024, "Common size [1024]")
 flags.DEFINE_float("learning_rate", 1e-2, "Learning rate [0.01]")
 flags.DEFINE_float("max_grad_norm", 40, "Max gradient norm during trainig [40]")
-flags.DEFINE_integer("num_layers", 3, "Number of LSTM layers [3]")
-flags.DEFINE_integer("hidden_size", 512, "Hidden size of LSTM [512]")
-flags.DEFINE_string("save_dir", "save", "Save path [save]")
-flags.DEFINE_string("log_dir", "log", "Summary path [log]")
-flags.DEFINE_boolean("bool_train", False, "Train? [False]")
-flags.DEFINE_boolean("draft", False, "Quick iteration of epochs? [False]")
-flags.DEFINE_integer("eval_num_batches", 50, "Number of batches to evaluate during training [50]")
+
+# training and testing options
+flags.DEFINE_boolean("is_train", False, "Train? [False]")
 flags.DEFINE_integer("eval_period", 3, "Evaluation period [3]")
-flags.DEFINE_integer("common_size", 1024, "Common size [1024]")
+flags.DEFINE_integer("eval_num_batches", 50, "Number of batches to evaluate during training [50]")
+flags.DEFINE_integer("save_period", 1, "Save period [1]")
+flags.DEFINE_string("save_dir", "save", "Save path [save]")
+flags.DEFINE_string("log_dir", "log", "Log path [log]")
+
+# for debugging
+flags.DEFINE_boolean("draft", False, "Quick iteration of epochs? [False]")
 
 FLAGS = flags.FLAGS
 
 def main(_):
     vocab_dict = json.load(open(FLAGS.vocab_dict, 'rb'))
     FLAGS.vocab_size = len(vocab_dict)
-    print "vocab size: %d" % len(vocab_dict)
+    pprint(FLAGS.__dict__)
 
     train_data_set = read_vqa(FLAGS.train_batch_size, FLAGS.train_image_rep_h5, FLAGS.train_image_idx,
                               FLAGS.train_sent_h5, FLAGS.train_len, FLAGS.train_label)
@@ -49,52 +57,34 @@ def main(_):
     FLAGS.num_mcs = train_data_set.num_mcs
     val_data_set = read_vqa(FLAGS.val_batch_size, FLAGS.val_image_rep_h5, FLAGS.val_image_idx, FLAGS.val_sent_h5, FLAGS.val_len, FLAGS.val_label)
 
-    save_dir = FLAGS.save_dir
-    if save_dir and not os.path.exists(save_dir):
-        os.mkdir(save_dir)
+    if not os.path.exists(FLAGS.save_dir):
+        os.mkdir(FLAGS.save_dir)
 
     # Time-sensitive parameters. Will be altered if draft.
-    train_num_batches = train_data_set.num_batches
-    eval_num_batches = FLAGS.eval_num_batches
-    val_num_batches = val_data_set.num_batches
-    num_epochs = FLAGS.num_epochs
-    eval_period = FLAGS.eval_period
     if FLAGS.draft:
-        train_num_batches = 5
-        eval_num_batches = 1
-        val_num_batches = 5
-        num_epochs = 5
-        eval_period = 1
+        FLAGS.train_num_batches = 5
+        FLAGS.eval_num_batches = 1
+        FLAGS.val_num_batches = 5
+        FLAGS.num_epochs = 5
+        FLAGS.eval_period = 1
 
     tf_graph = tf.Graph()
-    train_model = Model(tf_graph, FLAGS, 'train')
-    test_model = Model(tf_graph, FLAGS, 'test')
     writer = tf.train.SummaryWriter(FLAGS.log_dir, tf_graph.as_graph_def())
+    model = Model(tf_graph, FLAGS, writer)
     with tf.Session(graph=tf_graph) as sess:
         sess.run(tf.initialize_all_variables())
-        if FLAGS.bool_train:
-            print "training %d epochs ..." % num_epochs
-            for epoch_idx in xrange(num_epochs):
-                train_model.train(sess, train_data_set, FLAGS.learning_rate, writer=writer, num_batches=train_num_batches)
-                if (epoch_idx + 1) % eval_period == 0:
-                    print "evaluating %d x %d examples (train data) ..." % (eval_num_batches, train_data_set.batch_size)
-                    test_model.test(sess, train_data_set, num_batches=eval_num_batches)
-                    print "evaluating %d x %d examples (val data) ..." % (eval_num_batches, val_data_set.batch_size)
-                    test_model.test(sess, val_data_set, writer=writer, num_batches=eval_num_batches)
-            print "saving model ..."
-            train_model.save(sess, FLAGS.save_dir)
+        if FLAGS.is_train:
+            model.train(sess, train_data_set, FLAGS.learning_rate, val_data_set=val_data_set)
         else:
-            print "loading model ..."
-            train_model.load(sess, FLAGS.save_dir)
+            model.load(sess)
+
+        print "-" * 80
+        print "training complete."
+        print "testing %d examples (train data) ..." % train_data_set.num_examples
+        model.test(sess, train_data_set, num_batches=FLAGS.train_num_batches)
 
         print "testing %d examples (val data) ..." % val_data_set.num_examples
-        test_model.test(sess, val_data_set, num_batches=val_num_batches)
-
-        print "testing %d examples (train data) ..." % train_data_set.num_examples
-        test_model.test(sess, train_data_set, num_batches=train_num_batches)
-
-
-
+        model.test(sess, val_data_set, num_batches=FLAGS.val_num_batches)
 
 if __name__ == "__main__":
     tf.app.run()
--- model.py
+++ model.py
@@ -9,149 +9,191 @@ from data import DataSet
 
 
 class Model(object):
-    def __init__(self, tf_graph, params, mode):
+    class Tensors(object):
+        pass
+
+    def __init__(self, tf_graph, params, writer, name=None):
         self.tf_graph = tf_graph
         self.params = params
-        self.mode = mode
+        self.save_dir = params.save_dir
+        self.name = name if name else self.__class__.__name__
+        self.writer = writer
         with tf_graph.as_default():
-            self._build_tf_graph()
+            self.train_tensors = self._build_tf_graph('train')
             self.saver = tf.train.Saver()
+            self.test_tensors = self._build_tf_graph('test')
 
-    def _build_tf_graph(self):
+
+    def _build_tf_graph(self, mode):
+        # Params
         params = self.params
-        num_layers = params.num_layers
-        hidden_size = params.hidden_size
+        rnn_num_layers = params.rnn_num_layers
+        rnn_hidden_size = params.rnn_hidden_size
         max_sent_size = params.max_sent_size
         image_rep_size = params.image_rep_size
         vocab_size = params.vocab_size
         common_size = params.common_size
-
-        summaries = []
-        global_step = tf.Variable(0, name="global_step", trainable=False)
-        self.global_step = global_step
-
-        if self.mode == 'train':
+        if mode == 'train':
             batch_size = params.train_batch_size
-        elif self.mode == 'test':
+        elif mode == 'test':
             batch_size = params.num_mcs
+        else:
+            raise Exception("Unknown mode: %s" % mode)
 
-        # placeholders
-        with tf.name_scope("%s/ph" % self.mode):
-            if self.mode == 'train':
-                learning_rate = tf.placeholder('float', name='lr')
-            input_sent_batch = tf.placeholder(tf.int32, [batch_size, max_sent_size], 'sent')
-            input_image_rep_batch = tf.placeholder(tf.float32, [batch_size, image_rep_size], 'image_rep')
-            input_len_batch = tf.placeholder(tf.int8, [batch_size], 'len')
-            target_batch = tf.placeholder(tf.int32, [batch_size, 2])
+        if mode == 'train': global_step = tf.Variable(0, name='global_step', trainable=False)
 
-        # input sent embedding
-        with tf.variable_scope("emb", reuse=self.mode=='test'):
-            emb_mat = tf.get_variable("emb_mat", [vocab_size, hidden_size])
-            x_batch = tf.nn.embedding_lookup(emb_mat, input_sent_batch)  # [N, d]
-
-        single_cell = rnn_cell.BasicLSTMCell(hidden_size, forget_bias=0.0)
-        cell = rnn_cell.MultiRNNCell([single_cell] * num_layers)
-        init_hidden_state = cell.zero_state(batch_size, tf.float32)
-
-        with tf.variable_scope('rnn', reuse=self.mode=='test'):
-            x_split_batch = [tf.squeeze(x_each_batch, [1])
-                             for x_each_batch in tf.split(1, max_sent_size, x_batch)]
-            o_split_batch, h_last_batch = rnn.rnn(cell, x_split_batch, init_hidden_state, sequence_length=input_len_batch)
-
-        with tf.variable_scope('trans', reuse=self.mode=='test'):
+        with tf.variable_scope('variable', reuse=(mode=='test')) as shared_scope:
+            emb_mat = tf.get_variable("emb_mat", [vocab_size, rnn_hidden_size])
             image_trans_mat = tf.get_variable("image_trans_mat", [image_rep_size, common_size])
             image_trans_bias = tf.get_variable("image_trans_bias", [1, common_size])
-            m_batch = tf.tanh(tf.matmul(input_image_rep_batch, image_trans_mat) + image_trans_bias)
-
-            sent_trans_mat = tf.get_variable("sent_trans_mat", [hidden_size, common_size])
+            sent_trans_mat = tf.get_variable("sent_trans_mat", [rnn_hidden_size, common_size])
             sent_trans_bias = tf.get_variable("sent_trans_bias", [1, common_size])
-            s_batch = tf.tanh(tf.matmul(tf.split(1, 2*num_layers, h_last_batch)[2*num_layers-1], sent_trans_mat) + sent_trans_bias)
-
-        # concatenate sent emb and image rep
-        with tf.variable_scope('out', reuse=self.mode=='test'):
-            # logit_batch = h_last_batch * m_batch
             class_mat = tf.get_variable("class_mat", [common_size, 2])
-            logit_batch = tf.matmul(s_batch * m_batch, class_mat)
 
-        with tf.name_scope('loss'):
-            losses = tf.nn.softmax_cross_entropy_with_logits(logit_batch, tf.cast(target_batch, 'float'))
-            avg_loss = tf.reduce_mean(losses)
-            summaries.append(tf.scalar_summary('%s_avg_loss' % self.mode, avg_loss))
+        with tf.name_scope(mode) as mode_scope:
+            summaries = []
 
-        self.input_sent_batch = input_sent_batch
-        self.input_image_batch = input_image_rep_batch
-        self.input_len_batch = input_len_batch
-        self.target_batch = target_batch
-        self.avg_loss = avg_loss
+            # placeholders
+            with tf.name_scope("placeholder"):
+                if mode == 'train': learning_rate = tf.placeholder('float', name='lr')
+                input_sent_batch = tf.placeholder(tf.int32, [batch_size, max_sent_size], 'sent')
+                input_image_rep_batch = tf.placeholder(tf.float32, [batch_size, image_rep_size], 'image_rep')
+                input_len_batch = tf.placeholder(tf.int8, [batch_size], 'len')
+                target_batch = tf.placeholder(tf.int32, [batch_size, 2], 'target')
 
-        if self.mode == 'train':
-            opt = tf.train.GradientDescentOptimizer(learning_rate)
-            grads_and_vars = opt.compute_gradients(losses)
-            clipped_grads_and_vars = [(tf.clip_by_norm(grad, params.max_grad_norm), var) for grad, var in grads_and_vars]
-            opt_op = opt.apply_gradients(clipped_grads_and_vars, global_step=global_step)
-            self.opt_op = opt_op
-            self.learning_rate = learning_rate
-        elif self.mode == 'test':
-            prob_batch = tf.reshape(tf.slice(logit_batch, [0, 1], [-1, 1]), [-1])
-            label_batch = tf.reshape(tf.slice(target_batch, [0, 1], [-1, 1]), [-1])
-            correct = tf.equal(tf.argmax(prob_batch, 0), tf.argmax(label_batch, 0))
-            self.correct = correct
-            self.prob_batch =prob_batch
-            self.label_batch = label_batch
+            # input sent embedding
+            x_batch = tf.nn.embedding_lookup(emb_mat, input_sent_batch, "emb")  # [N, d]
 
-        self.summary = tf.merge_summary(summaries)
+            with tf.name_scope('rnn'):
+                single_cell = rnn_cell.BasicLSTMCell(rnn_hidden_size, forget_bias=0.0)
+                cell = rnn_cell.MultiRNNCell([single_cell] * rnn_num_layers)
+                init_hidden_state = cell.zero_state(batch_size, tf.float32)
+                x_split_batch = [tf.squeeze(x_each_batch, [1])
+                                 for x_each_batch in tf.split(1, max_sent_size, x_batch)]
+                o_split_batch, h_last_batch = rnn.rnn(cell, x_split_batch, init_hidden_state,
+                                                      sequence_length=input_len_batch, scope=shared_scope)
 
-    def _get_feed_dict(self, image_rep_batch, sent_batch, len_batch, target_batch=None):
-        feed_dict = {self.input_image_batch: image_rep_batch,
-                     self.input_sent_batch: sent_batch,
-                     self.input_len_batch: len_batch}
+            with tf.name_scope('trans'):
+                m_batch = tf.tanh(tf.matmul(input_image_rep_batch, image_trans_mat) + image_trans_bias, 'image_trans')
+                sent_rep_batch = tf.split(1, 2*rnn_num_layers, h_last_batch)[2*rnn_num_layers-1]
+                s_batch = tf.tanh(tf.matmul(sent_rep_batch, sent_trans_mat) + sent_trans_bias, 'sent_trans')
+
+            # concatenate sent emb and image rep
+            logit_batch = tf.matmul(s_batch * m_batch, class_mat, name='logit')
+
+            with tf.name_scope('loss'):
+                losses = tf.nn.softmax_cross_entropy_with_logits(logit_batch, tf.cast(target_batch, 'float'))
+                total_loss = tf.add_n(losses, name='total_loss')
+                avg_loss = tf.reduce_mean(losses, name='avg_loss')
+                ema = tf.train.ExponentialMovingAverage(0.9, name='ema')
+                summaries.append(tf.scalar_summary(avg_loss.op.name, avg_loss))
+                summaries.append(tf.scalar_summary(avg_loss.op.name + '(moving)', ema.average(avg_loss)))
+
+            if mode == 'train':
+                with tf.name_scope('opt'):
+                    loss_averages_op = ema.apply(losses + [total_loss])
+                    # with tf.control_dependencies([loss_averages_op]):
+                    # opt = tf.train.GradientDescentOptimizer(learning_rate)
+                    opt = tf.train.AdagradOptimizer(learning_rate)
+                    grads_and_vars = opt.compute_gradients(losses)
+                    # clipped_grads_and_vars = [(tf.clip_by_norm(grad, params.max_grad_norm), var) for grad, var in grads_and_vars]
+                    opt_op = opt.apply_gradients(grads_and_vars, global_step=global_step)
+
+            elif mode == 'test':
+                with tf.name_scope('eval'):
+                    prob_batch = tf.reshape(tf.slice(logit_batch, [0, 1], [-1, 1]), [-1])
+                    label_batch = tf.reshape(tf.slice(target_batch, [0, 1], [-1, 1]), [-1])
+                    correct = tf.equal(tf.argmax(prob_batch, 0), tf.argmax(label_batch, 0))
+
+            else:
+                raise Exception("Unknown mode: %s" % mode)
+
+            merged_summary = tf.merge_summary(summaries)
+
+            # Storing variables
+            if mode == 'train':
+                self.global_step = global_step
+
+            # Storing tensors
+            tensors = Model.Tensors()
+            if mode == 'train':
+                tensors.opt_op = opt_op
+                tensors.learning_rate = learning_rate
+            elif mode == 'test':
+                tensors.correct = correct
+                tensors.prob_batch =prob_batch
+                tensors.label_batch = label_batch
+            else:
+                raise Exception("Unknown mode: %s" % mode)
+            tensors.input_sent_batch = input_sent_batch
+            tensors.input_image_batch = input_image_rep_batch
+            tensors.input_len_batch = input_len_batch
+            tensors.target_batch = target_batch
+            tensors.avg_loss = avg_loss
+            tensors.merged_summary = merged_summary
+
+    def _get_feed_dict(self, tensors, image_rep_batch, sent_batch, len_batch, target_batch=None):
+        feed_dict = {tensors.input_image_batch: image_rep_batch,
+                     tensors.input_sent_batch: sent_batch,
+                     tensors.input_len_batch: len_batch}
         if target_batch is not None:
-            feed_dict[self.target_batch] = target_batch
+            feed_dict[tensors.target_batch] = target_batch
         return feed_dict
 
     def train_batch(self, sess, image_rep_batch, sent_batch, len_batch, target_batch, learning_rate):
-        assert self.mode == 'train', "This model is not for training!"
-        feed_dict = self._get_feed_dict(image_rep_batch, sent_batch, len_batch, target_batch)
-        feed_dict[self.learning_rate] = learning_rate
-        return sess.run([self.opt_op, self.summary, self.global_step], feed_dict=feed_dict)
+        tensors = self.train_tensors
+        feed_dict = self._get_feed_dict(tensors, image_rep_batch, sent_batch, len_batch, target_batch)
+        feed_dict[tensors.learning_rate] = learning_rate
+        return sess.run([tensors.opt_op, tensors.merged_summary, self.global_step], feed_dict=feed_dict)
 
-    def train(self, sess, train_data_set, learning_rate, writer=None, num_batches=None):
-        if num_batches is None:
-            num_batches = train_data_set.num_batches
-        assert self.mode == 'train', 'This model is not for training!'
+    def train(self, sess, train_data_set, learning_rate, val_data_set=None):
         assert isinstance(train_data_set, DataSet)
         params = self.params
+        num_batches = params.train_num_batches
         batch_size = params.train_batch_size
         max_sent_size = params.max_sent_size
 
-        pbar = pb.ProgressBar(widgets=["epoch %d:" % (train_data_set.num_epochs_completed + 1),
-                                       pb.Percentage(), pb.Bar(), pb.ETA()], maxval=train_data_set.num_batches)
-        pbar.start()
-        for num_batches_completed in xrange(num_batches):
-            image_rep_batch, mc_sent_batch, mc_len_batch, mc_label_batch = train_data_set.get_next_labeled_batch()
-            sent_batch, len_batch, target_batch = np.zeros([batch_size, max_sent_size]), np.zeros([batch_size]), np.zeros([batch_size, 2])
-            for i, (mc_sent, mc_len, mc_label) in enumerate(zip(mc_sent_batch, mc_len_batch, mc_label_batch)):
-                correct_idx = np.argmax(mc_label)
-                if np.random.randint(2) > 0:
-                    sent, len_, label = mc_sent[correct_idx], mc_len[correct_idx], mc_label[correct_idx]
-                else:
-                    delta_idx = np.random.randint(params.num_mcs-1) + 1
-                    new_idx = correct_idx - delta_idx
-                    sent, len_, label = mc_sent[new_idx], mc_len[new_idx], mc_label[new_idx]
-                target = np.array([0, 1]) if label else np.array([1, 0])
-                sent_batch[i, :] = sent
-                len_batch[i] = len_
-                target_batch[i, :] = target
-            result = self.train_batch(sess, image_rep_batch, sent_batch, len_batch, target_batch, learning_rate)
-            summary_str, global_step = result[1], result[2]
-            if writer: writer.add_summary(summary_str, global_step)
-            pbar.update(num_batches_completed)
-        pbar.finish()
+        print "training %d epochs ..." % params.num_epochs
+        for epoch_idx in xrange(params.num_epochs):
+            pbar = pb.ProgressBar(widgets=["epoch %d:" % (train_data_set.num_epochs_completed + 1),
+                                           pb.Percentage(), pb.Bar(), pb.ETA()], maxval=train_data_set.num_batches)
+            pbar.start()
+            for num_batches_completed in xrange(num_batches):
+                image_rep_batch, mc_sent_batch, mc_len_batch, mc_label_batch = train_data_set.get_next_labeled_batch()
+                sent_batch, len_batch, target_batch = np.zeros([batch_size, max_sent_size]), np.zeros([batch_size]), np.zeros([batch_size, 2])
+                for i, (mc_sent, mc_len, mc_label) in enumerate(zip(mc_sent_batch, mc_len_batch, mc_label_batch)):
+                    correct_idx = np.argmax(mc_label)
+                    if np.random.randint(2) > 0:
+                        sent, len_, label = mc_sent[correct_idx], mc_len[correct_idx], mc_label[correct_idx]
+                    else:
+                        delta_idx = np.random.randint(params.num_mcs-1) + 1
+                        new_idx = correct_idx - delta_idx
+                        sent, len_, label = mc_sent[new_idx], mc_len[new_idx], mc_label[new_idx]
+                    target = np.array([0, 1]) if label else np.array([1, 0])
+                    sent_batch[i, :] = sent
+                    len_batch[i] = len_
+                    target_batch[i, :] = target
+                result = self.train_batch(sess, image_rep_batch, sent_batch, len_batch, target_batch, learning_rate)
+                summary_str, global_step = result[1], result[2]
+                self.writer.add_summary(summary_str, global_step)
+                pbar.update(num_batches_completed)
+            pbar.finish()
+            train_data_set.complete_epoch()
 
-        train_data_set.complete_epoch()
+            if val_data_set and (epoch_idx + 1) % params.eval_period == 0:
+                print "evaluating %d x %d examples (train data) ..." % (params.eval_num_batches, train_data_set.batch_size)
+                self.test(sess, train_data_set, num_batches=params.eval_num_batches)
+                print "evaluating %d x %d examples (val data) ..." % (params.eval_num_batches, val_data_set.batch_size)
+                self.test(sess, val_data_set, writer=writer, num_batches=params.eval_num_batches)
 
-    def test(self, sess, test_data_set, num_batches=None, writer=None):
+            if (epoch_idx + 1) % params.save_period == 0:
+                print "saving model ..."
+                self.save(sess)
+
+    def test(self, sess, test_data_set, num_batches=None):
         assert isinstance(test_data_set, DataSet)
+
+        tensors = self.test_tensors
 
         if num_batches is None:
             num_batches = test_data_set.num_batches
@@ -164,8 +206,9 @@ class Model(object):
             for image_rep, mc_sent, mc_len, mc_label in zip(image_rep_batch, mc_sent_batch, mc_len_batch, mc_label_batch):
                 mc_image_rep = np.tile(image_rep, [len(mc_sent), 1])
                 mc_target = np.array([[0, 1] if label else [1, 0] for label in mc_label])
-                feed_dict = self._get_feed_dict(mc_image_rep, mc_sent, mc_len, mc_target)
-                correct, each_avg_loss, summary_str, global_step= sess.run([self.correct, self.avg_loss, self.summary, self.global_step], feed_dict=feed_dict)
+                feed_dict = self._get_feed_dict(tensors, mc_image_rep, mc_sent, mc_len, mc_target)
+                correct, each_avg_loss, summary_str, global_step \
+                    = sess.run([tensors.correct, tensors.avg_loss, tensors.merged_summary, self.global_step], feed_dict=feed_dict)
                 num_corrects += correct
                 total_avg_loss += each_avg_loss
             pbar.update(num_batches_completed)
@@ -174,17 +217,15 @@ class Model(object):
         total = num_batches * test_data_set.batch_size
         acc = float(num_corrects)/total
         avg_loss = total_avg_loss/total
-        if writer: writer.add_summary(summary_str, global_step)
-        """
-        summary = tf.scalar_summary("eval_%s_avg_loss" % self.mode, avg_loss)
-        if writer: writer.add_summary(summary.eval(session=sess), global_step)
-        """
+        self.writer.add_summary(summary_str, self.global_step)
         print "%d/%d = %.4f, loss=%.4f" % (num_corrects, total, acc, avg_loss)
 
-    def save(self, sess, save_dir):
-        save_path = os.path.join(save_dir, 'model')
+    def save(self, sess):
+        print "saving model ..."
+        save_path = os.path.join(self.save_dir, self.name)
         self.saver.save(sess, save_path, self.global_step)
 
-    def load(self, sess, save_dir):
-        checkpoint = tf.train.get_checkpoint_state(save_dir)
+    def load(self, sess):
+        print "loading model ..."
+        checkpoint = tf.train.get_checkpoint_state(self.save_dir)
         self.saver.restore(sess, checkpoint.model_checkpoint_path)
