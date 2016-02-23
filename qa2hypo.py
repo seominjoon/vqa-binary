75a81a43fab0c3c404ad652b17d905ff4b5f16a1
Angli Liu <anglil@uw.edu>
2016-02-22 17:48:31 -0800

    sample

--- qa2hypo.py
+++ qa2hypo.py
@@ -2,35 +2,70 @@ import numpy as np
 import json
 import argparse
 import os
+import random
 
 parser = argparse.ArgumentParser()
 parser.add_argument('root_dir')
 ARGS = parser.parse_args()
 
-
-def qa2hypo(args):
+# prepare a qa_pairs json object and write it to disk
+def prepro_questions_annotations(args):
 	root_dir = args.root_dir
 	questions_path = os.path.join(root_dir, 'MultipleChoice_shining3_all_questions.json')
 	annotations_path = os.path.join(root_dir, 'shining3_all_annotations.json')
 
 	print "Loading json files ..."
-	#questions = json.load(open(questions_path, 'rb'))
+	questions = json.load(open(questions_path, 'rb'))
 	annotations = json.load(open(annotations_path, 'rb'))
 	
 	#return questions, annotations
 
 	qa_path = os.path.join(root_dir, 'qa_pairs.json')
 	qa_pairs = {}
+	qa_pairs['qa_pairs'] = []
+	for i in range(len(annotations['annotations'])):
+		qa_pair = {}
+		qa_pair['image_id'] = annotations['annotations'][i]['image_id']
+		qa_pair['ans'] = annotations['annotations'][i]['multiple_choice_answer']
+		qa_pair['question_id'] = annotations['annotations'][i]['question_id']
+		qa_pair['question'] = questions['questions'][i]['question']
+		qa_pairs['qa_pairs'].append(qa_pair)
 
 	print "Dumping json files ..."
 	json.dump(qa_pairs, open(qa_path, 'wb'))
 
+# turn qa_pairs into hypotheses
+def qa2hypo(args):
+	root_dir = args.root_dir
+	qa_path = os.path.join(root_dir, 'qa_pairs.json')
+
+	print "Loading json files ..."
+	qa_pairs = json.load(open(qa_path, 'rb'))
+	qa_pairs_list = qa_pairs['qa_pairs']
+
+	# number of samples
+	k = 5
+	qa_pairs_list_sampled = sample_qa(qa_pairs_list, k)
+	for i in qa_pairs_list_sampled:
+		print i
+
+
+# sample sentences
+def sample_qa(qa_pairs_list, k):
+	l = range(len(qa_pairs_list))
+	# random sampling
+	l_sampled = random.sample(l, k)
+	return [qa_pairs_list[i] for i in l_sampled]
 
 
 
 
 if __name__ == "__main__":
-	qa2hypo(ARGS)
+	#prepro_questions_annotations(ARGS)
+	qa_pairs = qa2hypo(ARGS)
+	# print len(qa_pairs['qa_pairs'])
+
+
 	#questions, annotations = read_questions_annotations(ARGS)
 
 	# for key in questions:
