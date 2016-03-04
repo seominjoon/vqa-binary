9ab6f2b9dc4fca8066fd6e45955d841a86367932
Minjoon Seo <seominjoon@gmail.com>
2016-03-03 23:51:28 -0800

    bug fix

--- download.sh
+++ download.sh
@@ -3,7 +3,7 @@ CM_DIR="$HOME/caffe-models"
 DQA_DATA_DIR="$HOME/data/dqa"
 
 mkdir $CM_DIR
-wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel -O $CM_DIR/VGG_ILVRC_19_layers.caffemodel
+wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel -O $CM_DIR/VGG_ILSVRC_19_layers.caffemodel
 wget https://gist.githubusercontent.com/ksimonyan/3785162f95cd2d5fee77/raw/f02f8769e64494bcd3d7e97d5d747ac275825721/VGG_ILSVRC_19_layers_deploy.prototxt -O $CM_DIR/VGG_ILSVRC_19_layers_deploy.prototxt
 
 mkdir $DQA_DATA_DIR
--- prepro_dqa.sh
+++ prepro_dqa.sh
@@ -1,23 +1,23 @@
 # Replace: PREFIX, EXT, DATA_DIR, DATA_TYPE, SUBTYPE, ROOT_DIR
-USER_DIR="/home/minjoons"
-PREFIX="shining3-1500_train_"
+USER_DIR="$HOME"
+PREFIX="shining3-1500r_train_"
 EXT=".png"
-DATA_DIR="data/dqa/shining3-1500-train-vqa"
-DATA_TYPE="shining3-1500"
+DATA_DIR="data/dqa/shining3-1500r-train-vqa"
+DATA_TYPE="shining3-1500r"
 SUBTYPE="train"
 IMAGES_DIR="train"
 ROOT_DIR="dqa-train"
 python prepro_common.py $USER_DIR/$DATA_DIR/MultipleChoice_"$DATA_TYPE"_"$SUBTYPE"_questions.json $USER_DIR/$DATA_DIR/"$DATA_TYPE"_"$SUBTYPE"_annotations.json $USER_DIR/$DATA_DIR/$IMAGES_DIR/ $ROOT_DIR --prefix $PREFIX --ext $EXT
 python prepro_questions.py $ROOT_DIR
-# th prepro_images.lua --image_path_json $ROOT_DIR/image_path.json --cnn_proto $USER_DIR/caffe-models/VGG_ILSVRC_19_layers_deploy.prototxt --cnn_model $USER_DIR/caffe-models/VGG_ILSVRC_19_layers.caffemodel --out_path $ROOT_DIR/image_rep.h5 --backend nn
+th prepro_images.lua --image_path_json $ROOT_DIR/image_path.json --cnn_proto $USER_DIR/caffe-models/VGG_ILSVRC_19_layers_deploy.prototxt --cnn_model $USER_DIR/caffe-models/VGG_ILSVRC_19_layers.caffemodel --out_path $ROOT_DIR/image_rep.h5 --backend nn
 VOCAB_DICT_PATH="$ROOT_DIR/vocab_dict.json"
-PREFIX="shining3-1500_test_"
+PREFIX="shining3-1500r_test_"
 EXT=".png"
-DATA_DIR="data/dqa/shining3-1500-test-vqa"
-DATA_TYPE="shining3-1500"
+DATA_DIR="data/dqa/shining3-1500r-test-vqa"
+DATA_TYPE="shining3-1500r"
 SUBTYPE="test"
 IMAGES_DIR="test"
 ROOT_DIR="dqa-test"
 python prepro_common.py $USER_DIR/$DATA_DIR/MultipleChoice_"$DATA_TYPE"_"$SUBTYPE"_questions.json $USER_DIR/$DATA_DIR/"$DATA_TYPE"_"$SUBTYPE"_annotations.json $USER_DIR/$DATA_DIR/$IMAGES_DIR/ $ROOT_DIR --prefix $PREFIX --ext $EXT
 python prepro_questions.py $ROOT_DIR --vocab_dict_path $VOCAB_DICT_PATH
-# th prepro_images.lua --image_path_json $ROOT_DIR/image_path.json --cnn_proto $USER_DIR/caffe-models/VGG_ILSVRC_19_layers_deploy.prototxt --cnn_model $USER_DIR/caffe-models/VGG_ILSVRC_19_layers.caffemodel --out_path $ROOT_DIR/image_rep.h5 --backend nn
+th prepro_images.lua --image_path_json $ROOT_DIR/image_path.json --cnn_proto $USER_DIR/caffe-models/VGG_ILSVRC_19_layers_deploy.prototxt --cnn_model $USER_DIR/caffe-models/VGG_ILSVRC_19_layers.caffemodel --out_path $ROOT_DIR/image_rep.h5 --backend nn
