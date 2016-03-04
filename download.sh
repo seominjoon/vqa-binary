a2d8eb02a1f14f491c334b74a3fff8f117d27a27
Minjoon Seo <seominjoon@gmail.com>
2016-03-03 22:59:11 -0800

    bug fix in download

--- download.sh
+++ download.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-CM_DIR="~/caffe-models"
-DQA_DATA_DIR="~/data/dqa"
+CM_DIR="$HOME/caffe-models"
+DQA_DATA_DIR="$HOME/data/dqa"
 
 mkdir $CM_DIR
-wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel $DL_PATH
-wget https://gist.githubusercontent.com/ksimonyan/3785162f95cd2d5fee77/raw/f02f8769e64494bcd3d7e97d5d747ac275825721/VGG_ILSVRC_19_layers_deploy.prototxt $DL_PATH
+wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_19_layers.caffemodel -O $CM_DIR/VGG_ILVRC_19_layers.caffemodel
+wget https://gist.githubusercontent.com/ksimonyan/3785162f95cd2d5fee77/raw/f02f8769e64494bcd3d7e97d5d747ac275825721/VGG_ILSVRC_19_layers_deploy.prototxt -O $CM_DIR/VGG_ILSVRC_19_layers_deploy.prototxt
 
 mkdir $DQA_DATA_DIR
-wget https://s3-us-west-2.amazonaws.com/dqa-data/shining3-1500r-vqa.zip $DQA_DATA_DIR
-unzip $DQA_DATA_DIR/shining3-1500r-vqa.zip $DQA_DATA_DIR
+wget https://s3-us-west-2.amazonaws.com/dqa-data/shining3-1500r-vqa.zip -O $DQA_DATA_DIR/shining3-1500r-vqa.zip
+unzip $DQA_DATA_DIR/shining3-1500r-vqa.zip -d $DQA_DATA_DIR
