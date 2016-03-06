0266c94d59baf24b1e854d7aeab43ebebb999b32
Minjoon Seo <seominjoon@gmail.com>
2016-03-05 22:11:33 -0800

    added porterstemmer

--- prepro_questions.py
+++ prepro_questions.py
@@ -7,6 +7,7 @@ from collections import Counter
 import progressbar as pb
 import h5py
 import numpy as np
+from nltk import PorterStemmer
 
 parser = argparse.ArgumentParser()
 parser.add_argument('root_dir')
@@ -16,6 +17,7 @@ parser.add_argument('--qa2hypo', default=0)
 
 ARGS = parser.parse_args()
 
+stemmer = PorterStemmer()
 
 def prepro_questions(args):
     root_dir = args.root_dir
@@ -26,7 +28,7 @@ def prepro_questions(args):
     vocab_dict_path = args.vocab_dict_path
     vocab_min_count = args.vocab_min_count
 
-    print "Loading json files ..."
+    print("Loading json files ...")
     question_list = json.load(open(question_list_path, 'rb'))
     multiple_choices_list = json.load(open(multiple_choices_list_path, 'rb'))
     answer_list = json.load(open(answer_list_path, 'rb'))
@@ -44,7 +46,7 @@ def prepro_questions(args):
 
     max_sent_len = 0
 
-    print "Preprocessing questions ..."
+    print("Preprocessing questions ...")
     num_questions = len(question_list)
     pbar = pb.ProgressBar(widgets=[pb.Percentage(), pb.Bar(), pb.Timer()], maxval=num_questions).start()
     for i, (raw_question, raw_mcs, raw_answer) in enumerate(zip(question_list, multiple_choices_list, answer_list)):
@@ -64,29 +66,27 @@ def prepro_questions(args):
             max_sent_len = tok_sent_len
 
         if not vocab_dict_path:
-            for tok in tok_question: vocab_counter[tok] += 1
+            for tok in tok_question: _vadd(vocab_counter, tok)
             for tok_mc in tok_mcs:
-                for tok in tok_mc: vocab_counter[tok] += 1
-            for tok in tok_answer: vocab_counter[tok] += 1
+                for tok in tok_mc: _vadd(vocab_counter, tok)
+            for tok in tok_answer: _vadd(vocab_counter, tok)
 
         pbar.update(i+1)
     pbar.finish()
 
     if create_vocab:
-        print "creating vocab dict ..."
-        vocab_list = zip(*sorted([pair for pair in vocab_counter.iteritems() if pair[1] > vocab_min_count],
-                                 key=lambda x: -x[1]))[0]
+        print("creating vocab dict ...")
+        vocab_list, _ = zip(*sorted([pair for pair in vocab_counter.items() if pair[1] > vocab_min_count],
+                                    key=lambda x: -x[1]))
 
         vocab_dict = {word: idx+1 for idx, word in enumerate(sorted(vocab_list))}
         vocab_dict['UNK'] = 0
-        print "vocab size: %d" % len(vocab_dict)
+        print("vocab size: %d" % len(vocab_dict))
 
-    def _get(word):
-        return vocab_dict[word] if word in vocab_dict else 0
 
-    print "Converting to numpy array ..."
+    print("Converting to numpy array ...")
     lens = [[len(each_tok_sent) for each_tok_sent in tok_sent] for tok_sent in tok_sents]
-    sents = [[[_get(word) for word in each_tok_sent] + [0] * (max_sent_len - len(each_tok_sent))
+    sents = [[[_vget(vocab_dict, word) for word in each_tok_sent] + [0] * (max_sent_len - len(each_tok_sent))
               for each_tok_sent in tok_sent] for tok_sent in tok_sents]
     sents = np.array(sents, dtype='int32')
 
@@ -98,12 +98,12 @@ def prepro_questions(args):
     len_path = os.path.join(root_dir, "len.json")
 
 
-    print "Dumping h5 file ..."
+    print("Dumping h5 file ...")
     f = h5py.File(sent_path, 'w')
     f['data'] = sents
     f.close()
 
-    print "Dumping json files ..."
+    print("Dumping json files ...")
     json.dump(labels, open(label_path, 'wb'))
     if create_vocab:
         json.dump(vocab_dict, open(vocab_dict_path, 'wb'))
@@ -114,6 +114,18 @@ def _tokenize(raw):
     tokens = re.findall(r"[\w]+", raw)
     tokens = [token.lower() for token in tokens]
     return tokens
+
+
+def _vadd(vocab_counter, word):
+    word = word.lower()
+    word = stemmer.stem(word)
+    vocab_counter[word] += 1
+
+
+def _vget(vocab_dict, word):
+    word = word.lower()
+    word = stemmer.stem(word)
+    return vocab_dict[word] if word in vocab_dict else 0
 
 
 def _append_answer(question, ans):
